68563551fcb700aec88d11d9cf74c5c1ba67438f
cgmpinos <92669146+cgmpinos@users.noreply.github.com>
2023-09-14 01:23:02 +0300

    Update EntryOfLeaf.java

--- EntryOfLeaf.java
+++ EntryOfLeaf.java
@@ -18,7 +18,56 @@ public class EntryOfLeaf extends Entry{
         this.recordId= recordId;
     }
 
+    /** Μέθοδος που επιστρέφει true αν η EntryOfLeaf κυριαρχεί επί μιας άλλης EntryOfLeaf που δέχεται σαν παράμετρο (gia skyline) */
+    public boolean dominatesAnotherEntryOfLeaf(EntryOfLeaf anotherEntryOfLeaf){
+        ArrayList<Double> coordinates = new ArrayList<>();
+        ArrayList<Double> anotherEntryOfLeafCoordinates = new ArrayList<>();
+        for(int i=0;i<this.getBoundingBox().getDimensions();i++){
+            coordinates.add(this.getBoundingBox().getBound(i,false));
+            anotherEntryOfLeafCoordinates.add(anotherEntryOfLeaf.getBoundingBox().getBound(i,false));
+        }
+        boolean entryOfLeafIsBetterThanAnotherEntryOfLeafInAtLeastOneAttribute = false;
 
+        for(int i=0;i<coordinates.size();i++){
+            if(anotherEntryOfLeafCoordinates.get(i)<coordinates.get(i)){
+                return false;
+            }
+            if(coordinates.get(i)<anotherEntryOfLeafCoordinates.get(i)){
+                entryOfLeafIsBetterThanAnotherEntryOfLeafInAtLeastOneAttribute = true;
+            }
+        }
+        if(entryOfLeafIsBetterThanAnotherEntryOfLeafInAtLeastOneAttribute){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    /** Μέθοδος που επιστρέφει true αν η EntryOfLeaf κυριαρχεί επί μιας άλλης Entry που δέχεται σαν παράμετρο (gia skyline) */
+    public boolean dominatesAnotherEntry(Entry anotherEntry){
+        ArrayList<Double> coordinates = new ArrayList<>();
+        ArrayList<Double> anotherEntryCoordinates = new ArrayList<>();
+        for(int i=0;i<this.getBoundingBox().getDimensions();i++){
+            coordinates.add(this.getBoundingBox().getBound(i,false));
+            anotherEntryCoordinates.add(anotherEntry.getBoundingBox().getBound(i,false));
+        }
+        boolean entryOfLeafIsBetterThanAnotherEntryOfLeafInAtLeastOneAttribute = false;
+
+        for(int i=0;i<coordinates.size();i++){
+            if(anotherEntryCoordinates.get(i)<coordinates.get(i)){
+                return false;
+            }
+            if(coordinates.get(i)<anotherEntryCoordinates.get(i)){
+                entryOfLeafIsBetterThanAnotherEntryOfLeafInAtLeastOneAttribute = true;
+            }
+        }
+        if(entryOfLeafIsBetterThanAnotherEntryOfLeafInAtLeastOneAttribute){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
     public RecordId getRecordId(){
         return recordId;
     }
